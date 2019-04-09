ac1f242d201cbd475323645077daeba226dbce32
munchj <munch.juu@gmail.com>
2019-04-09 06:54:03 +0000

    fixed config

--- dist/bundle.js
+++ dist/bundle.js
@@ -473,10 +473,10 @@ $(document).ready(function () {
 "use strict";
 
 module.exports = {
-  ws_base_handling: "ws://localhost:1337",
-  ws_base_camera: "ws://localhost:1338",
-  ws_turret_handling: "ws://localhost:1339",
-  ws_turret_camera: "ws://localhost:1340"
+  ws_base_handling: "ws:\/\/192.168.1.24:1337",
+  ws_base_camera: "ws:\/\/192.168.1.24:1338",
+  ws_turret_handling: "ws:\/\/192.168.1.22:1339",
+  ws_turret_camera: "ws:\/\/192.168.1.22:1340"
 };
 
 },{}],4:[function(require,module,exports){
